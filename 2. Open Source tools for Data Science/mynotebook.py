01a6dd0255b1022f7fecdbb29097eb9ba91c4970
Wai-Lin-Htet <104857416+Wai-Lin-Htet@users.noreply.github.com>
2024-08-01 01:41:01 +0630

    Update mynotebook.py

--- 2. Open Source tools for Data Science/mynotebook.py
+++ 2. Open Source tools for Data Science/mynotebook.py
@@ -1,5 +1,16 @@
 # Title of the Notebook
 ## Introduction
+'''
+Welcome to this Data Science Notebook! This notebook 
+is designed to showcase various fundamental aspects of data science, 
+including the popular languages and libraries used in the field, 
+as well as essential tools and arithmetic operations. 
+By working through the exercises, you will gain a deeper 
+understanding of the basic concepts and practices that 
+form the foundation of data science. Let's get started 
+on this journey to explore the fascinating world of data science!
+'''
+
 ## Data Science Languages
 '''
 - Python
@@ -26,6 +37,11 @@
 
 '''
 ## Arithmetic Expression Examples
+# Addition
+a = 5
+b = 3
+sum_result = a + b
+sum_result
 
 # Example of multiplication and addition
 result = (3 * 4) + 5
@@ -35,6 +51,12 @@ result
 minutes = 120
 hours = minutes / 60
 hours
+
+# Convert minutes to hours
+minutes = 150  # You can change this value to any number of minutes
+hours = minutes / 60
+hours
+
 
 
 ## Objectives
