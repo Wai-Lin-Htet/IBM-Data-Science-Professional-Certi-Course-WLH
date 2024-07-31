cd3293b1878736bee2dd16e97f78191c9f4770a3
Wai-Lin-Htet <104857416+Wai-Lin-Htet@users.noreply.github.com>
2024-08-01 01:21:02 +0630

    my notebook

---
+++ 2. Open Source tools for Data Science/mynotebook.py
@@ -0,0 +1,51 @@
+# Title of the Notebook
+## Introduction
+## Data Science Languages
+'''
+- Python
+- R
+- SQL
+- Julia
+- Java
+'''
+## Data Science Libraries
+'''
+- NumPy
+- Pandas
+- Matplotlib
+- Scikit-learn
+- TensorFlow
+'''
+## Data Science Tools
+'''
+| Tool             | Description                      |
+|------------------|----------------------------------|
+| Jupyter Notebook | Interactive computing environment|
+| RStudio          | Integrated development environment for R |
+| Apache Spark     | Cluster-computing framework      |
+
+'''
+## Arithmetic Expression Examples
+
+# Example of multiplication and addition
+result = (3 * 4) + 5
+result
+
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+hours
+
+
+## Objectives
+'''
+- Understand basic data science concepts
+- Familiarize with common data science tools and libraries
+- Perform basic arithmetic operations using Python
+'''
+
+## Author
+
+'''
+Wai Lin Htet
+'''
